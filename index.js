17b63168a07c28a8cdf1078b26fa16b6c9371775
Emily Valdez <emily.milan.valdez@gmail.com>
2023-08-30 11:41:54 -0500

    loop lap changes

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solutions in this file
+function writeCards(cards, string) {
+    let thankyouMessage = []
+    for (let i = 0; i < cards.length; i++) {
+      thankyouMessage.push(`Thank you, ${cards[i]}, for the wonderful `+string+` gift!`);
+    }
+    return thankyouMessage;
+}
+function countDown(n) {
+while (n >= 0) {
+  console.log(n--);
+}
+}
